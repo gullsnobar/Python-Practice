3a7dac5c6cdff93b51adb5c3d33f3024a33fc1ca
Gull Snobar <gullsnobar07@gmail.com>
2025-10-25 19:35:23 +0500

    Todays Practice

--- Sixthprogram.py
+++ Sixthprogram.py
@@ -1,1 +1,22 @@
-# print("I hate you 😡 \n" * 10);
+# print("I hate you 😡 \n" * 10)
+
+print("Hello from Snobar!")
+
+# Example 1
+x = 10
+if x > 5:
+    print("x is greater than 5")
+else:
+    print("x is less than or equal to 5")
+
+# Conditional Statements
+# Example 2
+x = 10
+if x > 20:
+    print("x is greater than 20")
+elif x > 15:
+    print("x is greater than 15 but less than or equal to 20")
+elif x > 10:
+    print("x is greater than 10 but less than or equal to 15")
+else:
+    print("x is less than or equal to 10")
